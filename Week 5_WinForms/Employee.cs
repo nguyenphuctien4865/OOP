f13784e45131d77451473a39f182fd4c91021964
Tien Nguyen Phuc <59163718+nguyenphuctien4865@users.noreply.github.com>
2021-09-26 21:13:37 +0700

    Add files via upload

---
+++ Week 5_WinForms/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private Date birthDate;
+        private Date hireDate;
+
+        public Employee(string first, string last , int birthMonth, int birthDay,int birthYear,int hireMonth,int hireDay, int hireYear)
+        {
+            firstName = first;
+            lastName = last;
+
+            birthDate = new Date(birthMonth, birthDay, birthYear);
+            hireDate = new Date(birthMonth, birthDay, birthYear);
+
+        }
+
+
+        public string ToEmployeeString()
+        {
+
+            return lastName + ", " + firstName + " Hired: " + hireDate.ToDateString() + " Birthday: " + birthDate.ToDateString();
+        }
+    }
+}
