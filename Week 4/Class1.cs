e62b3df423dd238966a1438d5c2c6cc53cef074d
Tien Nguyen Phuc <59163718+nguyenphuctien4865@users.noreply.github.com>
2021-09-26 21:09:31 +0700

    Add files via upload

---
+++ Week 4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week_4
+{
+    public class Class1
+    {
+        private int field;
+
+        public void Method()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
